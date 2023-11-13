ad90d64dc530aa8500e15f203e021c6bf23857be
Mohan Raj Rajkumar <144455486+mohanraj7rajkumar@users.noreply.github.com>
2023-11-13 19:06:39 +0000

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h> // Include the conio.h header for getch() function
+
+int main(void) 
+{
+    printf("Hello World\n"); // Print "Hello, World" to the console with a newline
+
+    getch(); // Wait for a key press
+    return 0; // Exit the program with a return code of 0 (indicating successful execution)
+}
